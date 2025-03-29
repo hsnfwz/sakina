ee5e9504881a61189a54668868bf4145ad6e7867
Hussein Fawaz <husseinfawaz@MacBook-Pro-7.local>
2025-03-28 19:36:48 -0700

    fixed sign up page

--- src/layouts/SignUp.jsx
+++ src/layouts/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import {
   expectedUsernameFormat,
